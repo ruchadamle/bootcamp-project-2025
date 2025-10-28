4285bbe3758e9557b850a05cb51082bd5b3a7478
Rucha Damle <ruchadamle@email.com>
2025-10-27 17:53:32 -0700

    feat: create navbar in React

---
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import style from "./navbar.module.css";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className={style.navbar}>
+      <h1 className={style.logo}>
+        <Link href="/">Rucha Damle</Link>
+      </h1>
+      <ul className={style.navList}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/resume">Resume</Link>
+        </li>
+        <li>
+          <Link href="/about">Contact</Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
