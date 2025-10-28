8b79e50cfdff514ecef12d8c83c16cac34b672b2
Rucha Damle <ruchadamle@email.com>
2025-10-27 18:40:50 -0700

    feat: add footer

---
+++ bootcamp-milestone-2/src/components/footer.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      © 2025 Rucha Damle&apos;s Personal Website 🐱 | All Rights Reserved
+    </footer>
+  );
+}
